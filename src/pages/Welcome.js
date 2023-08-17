6c92375ee3ca6a8bc2039167686ed33f9aa7ffe8
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-08-16 20:43:12 -0400

    welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,14 @@
 // Welcome Page
 import React from "react";
-// import Header from "../components/Header";
-// import Footer from "../components/Footer";
+import selfie from '../assets/images/selfie.jpg'
 
 const Welcome = () => {
     return (
       <main>
-        <h1>Add a description about what I'm doing</h1>
+        <h1 className="text-center">Welcome to my portfolio.  Please take a look around and learn a little about me, what I have done, and how I work.</h1>
+        <div className="selfie">
+          <img src={selfie}  width="500" height="800"/>
+        </div>
       </main>
     );
   };
