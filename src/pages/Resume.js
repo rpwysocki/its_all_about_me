3659b095aab1fd9f34b697282b350e42226dec57
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-10-25 12:09:17 -0400

    resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Resume = () => {
   return (
-    <div>
+    <div className='resume'>
       <h1 className='text-center'>Resume Coming Soon!</h1>
     </div>
   );
